838beca7bfee93cc1b97af394bcd788b60f2062f
benehiebl <112540446+benehiebl@users.noreply.github.com>
2024-07-09 15:48:06 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,3 @@
-import xarray as xr
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-import pydeck as pdk
-#plt.style.use(["science"])
-
-import scienceplots
-import rasterio
 import geopandas as gpd
 import leafmap.foliumap as leafmap
 
@@ -37,20 +26,13 @@ st.markdown("- forest type maps were produced based on Italian Forest Vegetation
             "- cover maps were produced using the Vegetation Plot observation collected in Sibillini and Gennargentu Nationalpark and an aggregated annual Sentinel-2 time series\n\n" \
         "NOTE: drag the forest type legend to avoid overlaps...")
 
-#sel = st.multiselect(label="Select forest types that should be masked from the map:",
-#                     options=labels)
 
 
+img_sel = st.checkbox("Show phenology in full size")
 
-
-# add filters
-#with st.echo():
 
 m2 = leafmap.Map(basemap="Esri.WorldImagery")#, height="1000px", width="1500px")
 m2.add_basemap("Esri.WorldImagery")
-
-
-
 
 
 m2.add_raster(class_name ,
@@ -86,7 +68,8 @@ m2.add_gdf(parks, layer_name="Parks", style_callback=lambda x: style)
 vpos = gpd.read_file(vpo_name)
 #style = {"fillColor": "#00000000"}
 m2.add_gdf(vpos, layer_name="VPOs")#, style_callback=lambda x: style)
-#m2.add_circle_markers_from_xy(vpos, popup="img")
+if img_sel:
+            m2.add_circle_markers_from_xy(vpos, popup="img")
 
 m2.add_legend(title="Forest Type", labels=labels, colors=colors, draggable=True, position="topright")
 m2.add_colormap(label="Cover %", cmap="Greens", vmin=0, vmax=100, position="bottomright", transparent=True)
@@ -95,36 +78,6 @@ try:
         m2.add_inspector_gui(position='topright', opened=True)
 except: pass
 
-#legend1 = """
-#<div style="position: fixed; bottom: 50px; left: 10px; width: 150px; height: 150px; 
-#            border:2px solid grey; z-index:9999; font-size:14px; background-color:white;">
-#    <p style="margin: 5px;">Legend 1</p>
-#    <i style="background: #FF0000"></i> 0<br>
-#    <i style="background: #00FF00"></i> 1<br>
-#    <i style="background: #0000FF"></i> 2<br>
-#    <i style="background: #FFFF00"></i> 3<br>
-#    <i style="background: #FF00FF"></i> 4<br>
-#    <i style="background: #00FFFF"></i> 5<br>
-#    <i style="background: #000000"></i> 6<br>
-#</div>
-#"""
-#
-#legend2 = """
-#<div style="position: fixed; bottom: 50px; left: 170px; width: 150px; height: 150px; 
-#            border:2px solid grey; z-index:9999; font-size:14px; background-color:white;">
-#    <p style="margin: 5px;">Legend 2</p>
-#    <i style="background: #FF0000"></i> 0<br>
-#    <i style="background: #00FF00"></i> 1<br>
-#    <i style="background: #0000FF"></i> 2<br>
-#    <i style="background: #FFFF00"></i> 3<br>
-#    <i style="background: #FF00FF"></i> 4<br>
-#    <i style="background: #00FFFF"></i> 5<br>
-#    <i style="background: #000000"></i> 6<br>
-#</div>
-#"""
-#
-## Add legends to the map
-#m2.get_root().html.add_child(folium.Element(legend1))
-#m2.get_root().html.add_child(folium.Element(legend2))
+
 
 m2.to_streamlit()
